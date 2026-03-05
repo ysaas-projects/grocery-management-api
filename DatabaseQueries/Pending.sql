8806fba57588a67c7dcc6614641c4f345a53cfdd
ShubhangiGund9 <shubhangigund15@gmail.com>
2026-03-05 09:39:59 +0530

    tables added pending.sql

--- DatabaseQueries/Pending.sql
+++ DatabaseQueries/Pending.sql
@@ -74,3 +74,67 @@ CREATE TABLE ProductImages
 
     -- CONSTRAINT UQ_Product_PrimaryImage UNIQUE (ProductId, IsPrimary) WHERE IsPrimary = 1
 );
+
+
+CREATE TABLE ProductBatches
+(
+    BatchId INT IDENTITY PRIMARY KEY,
+    FirmId INT NULL,
+    ProductId INT NOT NULL,
+    BatchNumber NVARCHAR(50) NULL,
+    MRP DECIMAL(10,2) NOT NULL,
+    CostPrice DECIMAL(10,2) NOT NULL,
+    SalePrice DECIMAL(10,2) NOT NULL,
+    Quantity DECIMAL(18,3) NOT NULL,
+    RemainingQty DECIMAL(18,3) NOT NULL,
+    ManufactureDate DATE NULL,
+    ExpiryDate DATE NULL,
+	IsDeleted BIT NOT NULL DEFAULT 0,
+    CreatedAt DATETIME DEFAULT GETDATE(),
+	UpdatedAt DATETIME NULL,
+    DeletedAt DATETIME NULL
+	    --CONSTRAINT FK_ProductBatches_Firms FOREIGN KEY (FirmId) REFERENCES Firms(FirmId),
+		-- CONSTRAINT FK_ProductBatches_Products FOREIGN KEY (ProductId) REFERENCES Products(ProductId),
+
+);
+CREATE TABLE StockTransactions
+(
+    TransactionId INT IDENTITY PRIMARY KEY,
+    FirmId INT NULL,
+    ProductId INT NOT NULL,
+    BatchId INT NULL,
+    TransactionType VARCHAR(20) NOT NULL,
+    Quantity DECIMAL(18,3) NOT NULL,
+    IsIncrease BIT NOT NULL,
+    ReferenceId INT NULL,
+    ReferenceType VARCHAR(20) NULL,
+    Notes NVARCHAR(500),
+    CreatedBy INT NULL,
+	CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    UpdatedAt DATETIME NULL,
+    DeletedAt DATETIME NULL
+
+	-- CONSTRAINT FK_StockTransaction_ProductBatches FOREIGN KEY (BatchId) REFERENCES ProductBatches(BatchId),
+	--CONSTRAINT FK_StockTransaction_Firms FOREIGN KEY (FirmId) REFERENCES Firms(FirmId),
+	-- CONSTRAINT FK_StockTransaction_Products FOREIGN KEY (ProductId) REFERENCES Products(ProductId),
+);
+
+CREATE TABLE StockAdjustments
+(
+    AdjustmentId INT IDENTITY PRIMARY KEY,
+    FirmId INT NULL,
+    ProductId INT NOT NULL,
+    BatchId INT NULL,
+    AdjustmentType VARCHAR(20),
+    Quantity DECIMAL(18,3),
+    Reason NVARCHAR(500),
+    CreatedBy INT NULL,
+	CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    UpdatedAt DATETIME NULL,
+    DeletedAt DATETIME NULL
+
+  --CONSTRAINT FK_StockAdjustment_Firms FOREIGN KEY (FirmId) REFERENCES Firms(FirmId),
+  -- CONSTRAINT FK_StockAdjustment_Products FOREIGN KEY (ProductId) REFERENCES Products(ProductId),
+  	-- CONSTRAINT FK_StockAdjustment_ProductBatches FOREIGN KEY (BatchId) REFERENCES ProductBatches(BatchId),
+
+);
